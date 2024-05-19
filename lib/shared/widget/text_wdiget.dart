1287f1bf0e852eb7e21f3a9cbce688106636f99f
Tetsukick <endeavor.0329@gmail.com>
2024-05-19 17:58:57 +0700

    change to selectable

--- lib/shared/widget/text_wdiget.dart
+++ lib/shared/widget/text_wdiget.dart
@@ -1,198 +1,227 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:bintango_indonesian_translater/shared/constants/color_constants.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class TextWidget {
   static Widget titleRedMedium(String data, {int maxLines = 1}) {
-    return AutoSizeText(
-      data,
-      maxLines: maxLines,
-      minFontSize: 11,
-      maxFontSize: 16,
-      style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          color: ColorConstants.primaryRed900,
-          fontSize: 14
+    return SelectionArea(
+      child: AutoSizeText(
+        data,
+        maxLines: maxLines,
+        minFontSize: 11,
+        maxFontSize: 16,
+        style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: ColorConstants.primaryRed900,
+            fontSize: 14
+        ),
       ),
     );
   }
 
   static Widget titleRedLargestBold(String data, {int maxLines = 1}) {
-    return AutoSizeText(
-      data,
-      maxLines: maxLines,
-      minFontSize: 24,
-      maxFontSize: 32,
-      style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          color: ColorConstants.primaryRed900,
-          fontSize: 32
+    return SelectionArea(
+      child: AutoSizeText(
+        data,
+        maxLines: maxLines,
+        minFontSize: 24,
+        maxFontSize: 32,
+        style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: ColorConstants.primaryRed900,
+            fontSize: 32
+        ),
       ),
     );
   }
 
   static Widget titleBlackLargeBold(String data, {int maxLines = 1}) {
-    return AutoSizeText(
-      data,
-      maxLines: maxLines,
-      minFontSize: 16,
-      maxFontSize: 24,
-      style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          color: ColorConstants.fontBlack,
-          fontSize: 24
+    return SelectionArea(
+      child: AutoSizeText(
+        data,
+        maxLines: maxLines,
+        minFontSize: 16,
+        maxFontSize: 24,
+        style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: ColorConstants.fontBlack,
+            fontSize: 24
+        ),
       ),
     );
   }
 
   static Widget titleBlackLargestBold(String data, {int maxLines = 1}) {
-    return AutoSizeText(
-      data,
-      maxLines: maxLines,
-      minFontSize: 20,
-      maxFontSize: 28,
-      style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          color: ColorConstants.fontBlack,
-          fontSize: 28
+    return SelectionArea(
+      child: AutoSizeText(
+        data,
+        maxLines: maxLines,
+        minFontSize: 20,
+        maxFontSize: 28,
+        style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: ColorConstants.fontBlack,
+            fontSize: 28
+        ),
       ),
     );
   }
 
   static Widget titleBlackMediumBold(String data, {int maxLines = 1}) {
-    return AutoSizeText(
-      data,
-      maxLines: maxLines,
-      minFontSize: 14,
-      maxFontSize: 18,
-      style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          color: ColorConstants.fontBlack,
-          fontSize: 18
+    return SelectionArea(
+      child: AutoSizeText(
+        data,
+        maxLines: maxLines,
+        minFontSize: 14,
+        maxFontSize: 18,
+        style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: ColorConstants.fontBlack,
+            fontSize: 18
+        ),
       ),
     );
   }
 
   static Widget titleBlackSmallBold(String data, {int maxLines = 1}) {
-    return AutoSizeText(
-      data,
-      maxLines: maxLines,
-      minFontSize: 9,
-      maxFontSize: 14,
-      style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          color: ColorConstants.fontBlack,
-          fontSize: 12
+    return SelectionArea(
+      child: AutoSizeText(
+        data,
+        maxLines: maxLines,
+        minFontSize: 9,
+        maxFontSize: 14,
+        style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: ColorConstants.fontBlack,
+            fontSize: 12
+        ),
       ),
     );
   }
 
   static Widget titleGrayLargeBold(String data, {int maxLines = 1}) {
-    return AutoSizeText(
-      data,
-      maxLines: maxLines,
-      minFontSize: 16,
-      maxFontSize: 20,
-      style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          color: ColorConstants.fontGrey,
-          fontSize: 20
+    return SelectionArea(
+      child: AutoSizeText(
+        data,
+        maxLines: maxLines,
+        minFontSize: 16,
+        maxFontSize: 20,
+        style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: ColorConstants.fontGrey,
+            fontSize: 20
+        ),
       ),
     );
   }
 
   static Widget titleGrayMediumBold(String data, {int maxLines = 1}) {
-    return AutoSizeText(
-      data,
-      maxLines: maxLines,
-      minFontSize: 14,
-      maxFontSize: 20,
-      style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          color: ColorConstants.fontGrey,
-          fontSize: 20
+    return SelectionArea(
+      child: AutoSizeText(
+        data,
+        maxLines: maxLines,
+        minFontSize: 14,
+        maxFontSize: 20,
+        style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: ColorConstants.fontGrey,
+            fontSize: 20
+        ),
       ),
     );
   }
 
   static Widget titleGrayMedium(String data, {int maxLines = 1}) {
-    return AutoSizeText(
-      data,
-      maxLines: maxLines,
-      minFontSize: 14,
-      maxFontSize: 18,
-      style: const TextStyle(
-          fontWeight: FontWeight.normal,
-          color: ColorConstants.fontGrey,
-          fontSize: 18
+    return SelectionArea(
+      child: AutoSizeText(
+        data,
+        maxLines: maxLines,
+        minFontSize: 14,
+        maxFontSize: 18,
+        style: const TextStyle(
+            fontWeight: FontWeight.normal,
+            color: ColorConstants.fontGrey,
+            fontSize: 18
+        ),
       ),
     );
   }
 
   static Widget titleGrayMediumSmallBold(String data, {int maxLines = 1}) {
-    return AutoSizeText(
-      data,
-      maxLines: maxLines,
-      minFontSize: 14,
-      maxFontSize: 18,
-      style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          color: ColorConstants.fontGrey,
-          fontSize: 18
+    return SelectionArea(
+      child: AutoSizeText(
+        data,
+        maxLines: maxLines,
+        minFontSize: 14,
+        maxFontSize: 18,
+        style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: ColorConstants.fontGrey,
+            fontSize: 18
+        ),
       ),
     );
   }
 
   static Widget titleGraySmallBold(String data, {int maxLines = 1}) {
-    return AutoSizeText(
-      data,
-      maxLines: maxLines,
-      minFontSize: 11,
-      maxFontSize: 16,
-      style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          color: ColorConstants.fontGrey,
-          fontSize: 14
+    return SelectionArea(
+      child: AutoSizeText(
+        data,
+        maxLines: maxLines,
+        minFontSize: 11,
+        maxFontSize: 16,
+        style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: ColorConstants.fontGrey,
+            fontSize: 14
+        ),
       ),
     );
   }
 
   static Widget titleGraySmallest(String data, {int maxLines = 1}) {
-    return AutoSizeText(
-      data,
-      maxLines: maxLines,
-      minFontSize: 9,
-      maxFontSize: 12,
-      style: const TextStyle(
-          color: ColorConstants.fontGrey,
-          fontSize: 12
+    return SelectionArea(
+      child: AutoSizeText(
+        data,
+        maxLines: maxLines,
+        minFontSize: 9,
+        maxFontSize: 12,
+        style: const TextStyle(
+            color: ColorConstants.fontGrey,
+            fontSize: 12
+        ),
       ),
     );
   }
 
   static Widget titleGraySmall(String data, {int maxLines = 1}) {
-    return AutoSizeText(
-      data,
-      maxLines: maxLines,
-      minFontSize: 9,
-      maxFontSize: 16,
-      style: const TextStyle(
-          color: ColorConstants.fontGrey,
-          fontSize: 14
+    return SelectionArea(
+      child: AutoSizeText(
+        data,
+        maxLines: maxLines,
+        minFontSize: 9,
+        maxFontSize: 16,
+        style: const TextStyle(
+            color: ColorConstants.fontGrey,
+            fontSize: 14
+        ),
       ),
     );
   }
 
   static Widget titleWhiteLargeBold(String data, {int maxLines = 1}) {
-    return AutoSizeText(
-      data,
-      maxLines: maxLines,
-      minFontSize: 16,
-      maxFontSize: 28,
-      style: const TextStyle(
-          fontWeight: FontWeight.bold,
-          color: ColorConstants.fontWhite,
-          fontSize: 24,
+    return SelectionArea(
+      child: AutoSizeText(
+        data,
+        maxLines: maxLines,
+        minFontSize: 16,
+        maxFontSize: 28,
+        style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: ColorConstants.fontWhite,
+            fontSize: 24,
+        ),
       ),
     );
   }
@@ -205,14 +234,16 @@ class TextWidget {
       ),
       child: Padding(
         padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 12),
-        child: AutoSizeText(
-          data,
-          maxLines: maxLines,
-          maxFontSize: 16,
-          style: const TextStyle(
-              fontWeight: FontWeight.bold,
-              color: ColorConstants.fontWhite,
-              fontSize: 14,
+        child: SelectionArea(
+          child: AutoSizeText(
+            data,
+            maxLines: maxLines,
+            maxFontSize: 16,
+            style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                color: ColorConstants.fontWhite,
+                fontSize: 14,
+            ),
           ),
         ),
       ),
